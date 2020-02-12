05c1aee39155d88c768404974884f1bc8bea0742
Ahmed Musafi Abir <43444792+abir6431@users.noreply.github.com>
2020-02-12 16:18:35 -0500

    Update csv-file.py

--- csv-file.py
+++ csv-file.py
@@ -3,8 +3,7 @@
 """
 This code does the following:
     1. opens a csv file
-    2. goes through each and every line of it and removes all lines that start with alphabets
-    3. Converts the 'string' type text of csv file to float
+    2. removes rows that start with alphabets
     4. writes it to a new file
 """
 
@@ -14,18 +13,20 @@ file=input('filename=')                                                         
 csvI=open(file,'r')                                                             #open csv file in read mode
 csvO=open(file+str('stripped'),'w',newline='')                                  #writer object to write csv
 csvR=csv.reader(csvI,delimiter=',')                                             #csv reader object with set delimiter: ,
-csvW=csv.writer(csvO)
+csvW=csv.writer(csvO)                                                           #csv writer object
+###This function tries to make the s value into float type. If the s value has alphabets it returns an error thus the function
+###will return False
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False 
+##############################################################################                                                          
+
 for row in csvR:
     for column in row:
-        ###This block of code tests/tries to make the column value into float. Remember even numbers in a csv file
-        ###is of type string. If the column value has digits then they can be made float, otherwise it returns an error
-        try:
-            float(column)
-            csvW.writerow(row)
-        except ValueError:                                                      #ValueError represents the error
-            continue                                                            # Means do nothing
-        ###Try block ends here
-csvO.close()                                                                    #closes and saves the writer file
-    
-    
-
+        if is_number(column)==True:                                             #returns True is column value is number
+            csvW.writerow(row)                                                  #write this whole row down to the csv file
+            break                                                               #return to the 'row' for loop
+csvO.close()   
